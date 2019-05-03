491569c78792436557e13737031766a560e4c5bc
Ryan <rjw1428@gmail.com>
2019-05-03 16:13:21 -0400

    removed food truck event

--- src/app/events/events.component.ts
+++ src/app/events/events.component.ts
@@ -8,18 +8,16 @@ import { Component, OnInit } from '@angular/core';
 export class EventsComponent implements OnInit {
   pageTitle = "Upcoming Events"
   sideTitle = "EVENTS"
-  eventTitle: string[] = ["LUPE'S SPECIAL", "HAPPY HOUR", "ON THE ROAD"]
+  eventTitle: string[] = ["LUPE'S SPECIAL", "HAPPY HOUR"]
   displayDescription: boolean = false;
   activeItem: number;
   img: string[] = [
     'https://firebasestorage.googleapis.com/v0/b/brodys-192703.appspot.com/o/site-images%2Fburger2.jpg?alt=media&token=a9f28608-adc9-4399-b896-4008f9c2b6e1',
-    'https://firebasestorage.googleapis.com/v0/b/brodys-192703.appspot.com/o/site-images%2Fflight.png?alt=media&token=6fc7b676-d2f8-41c0-bdb7-7019ac26ec0a',
-    'https://firebasestorage.googleapis.com/v0/b/brodys-192703.appspot.com/o/site-images%2Ftruck.jpg?alt=media&token=5cd1f40a-5bc5-478e-95cd-d04f41b1c043']
-  
+    'https://firebasestorage.googleapis.com/v0/b/brodys-192703.appspot.com/o/site-images%2Fflight.png?alt=media&token=6fc7b676-d2f8-41c0-bdb7-7019ac26ec0a']
+
   description: string[]=[
     'This is the special of the week',
-    'Happy hour 4-6 pm every day, note some of our beers are not available during happy hour',
-    'Come meet us on the road at our next event'
+    'Happy hour 4-6 pm every day, note some of our beers are not available during happy hour'
   ]
   constructor() { }
 
