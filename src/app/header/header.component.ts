e52e620e2d7ef573a9a1ed9fe979968a038848cc
rjw1428 <rjw1428@gmail.com>
2020-02-15 18:49:40 -0500

    added favicons... upgrade to angular 8... minor cosmetic improvements

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -9,7 +9,7 @@ export class HeaderComponent implements OnInit {
   phoneNum = "619.303.5231"
   address = "12930 Campo Rd Jamul, CA"
   fb = "https://www.facebook.com/pages/Brodys-Burgers-Beer/329168860444473"
-  twit = "https://twitter.com/brodysbb12930"
+  twit = "https://twitter.com/BrodysBurgers"
   ig = "http://instagram.com/brodysburgersandbeer"
   map="https://www.google.com/maps/place/Brody's+Burgers+%26+Beer/@32.7286634,-116.9110995,17z/data=!3m1!4b1!4m5!3m4!1s0x80d95b7427429539:0xadf5aa7334345b70!8m2!3d32.7286589!4d-116.9089108"
   constructor() { }
