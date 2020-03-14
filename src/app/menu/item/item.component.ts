d7deaea335a0e228c725add9c432f9db084ca408
rjw1428 <rjw1428@gmail.com>
2020-03-14 15:25:51 -0400

    added price (data structure is ass... but, its fine I guess

--- src/app/menu/item/item.component.ts
+++ src/app/menu/item/item.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-item',
@@ -6,10 +7,20 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./item.component.css']
 })
 export class ItemComponent implements OnInit {
-  @Input() item: {name: string, description: string, img: any, price: number}
+  currencyPipe = new CurrencyPipe('en-US')
+  @Input() item: {name: string, description: string, img: any, price: any, priceString: string}
   constructor() { }
 
   ngOnInit() {
+    this.item.priceString=""
+    if (this.item.price) {
+      Object.keys(this.item.price).forEach(key=>{
+        if (Object.keys(this.item.price).length>1) {
+          this.item.priceString += `${key} ${this.currencyPipe.transform(this.item.price[key])} `
+        }
+        else  this.item.priceString += `${this.currencyPipe.transform(this.item.price[key])} `
+      })
+    }
   }
 
 }
